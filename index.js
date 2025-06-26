c31266691ff894c13cf382813a5f6ca2a37e56c1
Your Name <you@example.com>
2025-06-26 18:18:16 +0200

    fix uninstall dotenv

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 console.log('Hello Boolean');
 
-import dotenv from 'dotenv';
-dotenv.config();
+// dotenv
+
+/* import dotenv from 'dotenv';
+dotenv.config(); */
+
 console.log(process.env.PASSWORD);
 
+
+// axios
+import axios from 'axios';
+
+// chalk
+import chalk from 'chalk';
